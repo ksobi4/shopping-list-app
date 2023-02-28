a72e57a5748c9fa2e3291e88297ed19ddcf770d7
Filip Ksobiak <filip456781337@gmail.com>
2023-02-28 10:23:36 +0100

    add home_remote_repo

--- lib/features/home/data/home_remote_repo.dart
+++ lib/features/home/data/home_remote_repo.dart
@@ -1,27 +1,44 @@
+import 'package:flutter/foundation.dart';
+import 'package:shopping_list/features/home/model/catalog.dart';
 import 'package:shopping_list/utils/auth.dart';
 import 'package:shopping_list/utils/database.dart';
 
 import '../../../utils/logger.dart';
 import '../../auth/model/user.dart';
-import '../model/list.dart';
+import '../model/item.dart';
 
 class HomeRemoteRepo {
   Future<List<Catalog>> getCatalogs() async {
-    var snap = await db.collection('users').doc(getUserId()).get();
+    String userID = getUserId();
+    var snap = await db.collection('users').doc(userID).get();
 
     var jsonData = snap.data() as Map<String, dynamic>;
 
     User user = User.fromJson(jsonData);
 
-    List<Catalog> outputList;
+    List<Catalog> outputList = [];
 
     for (var catalogId in user.catalogsIdList) {
       var snap = await db.collection('catalogs').doc(catalogId).get();
-      var jsonData = snap.data();
+      var jsonData = snap.data() as Map<String, dynamic>;
 
-      logger.i('output: $jsonData');
+      String name = jsonData['name'];
+
+      var snap1 = await db
+          .collection('catalogs')
+          .doc(catalogId)
+          .collection('items')
+          .get();
+
+      List<Item> itemList = [];
+      for (var el in snap1.docs) {
+        itemList.add(Item.fromJson(el.data()));
+      }
+
+      logger.i('name: $name');
+      outputList.add(Catalog(name: name, items: itemList));
     }
 
-    return [];
+    return outputList;
   }
 }
